32f60ad2d84c394f0c480e41b3b6b39f8df85d6c
Vladimir <lo_0c@mail.ru>
2022-07-08 11:13:55 +0300

    db-2

--- server/db/seeders/20220708080311-clients.js
+++ server/db/seeders/20220708080311-clients.js
@@ -1,24 +1,15 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Clients', [
+      {
+        check_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Clients', null, {});
+  },
 };
--- server/db/seeders/20220708080335-orders.js
+++ server/db/seeders/20220708080335-orders.js
@@ -1,24 +1,38 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Orders', [
+      {
+        check_id: 1,
+        product_id: 1,
+        counter: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        check_id: 1,
+        product_id: 2,
+        counter: 7,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        check_id: 1,
+        product_id: 3,
+        counter: 10,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        check_id: 1,
+        product_id: 4,
+        counter: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Orders', null, {});
+  },
 };

9c52bcb67cc21a6835925de542655744f6a276a5
Albert Revazov <trezeguet15@mail.ru>
2022-07-09 11:24:26 +0300

    test2

--- server/app.js
+++ server/app.js
@@ -9,17 +9,16 @@ const Bcrypt = require('./utils/bcrypt');
 
 const app = express();
 
-const menuRouter = require('./routers/menuRouter')
-const orderRouter = require('./routers/orderRouter')
+const menuRouter = require('./routers/menuRouter');
+const orderRouter = require('./routers/orderRouter');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(process.env.PWD, 'public')));
 
-
-app.use('/', menuRouter)
-app.use('/order', orderRouter)
+app.use('/', menuRouter);
+app.use('/order', orderRouter);
 
 const sessionConfig = {
   name: 'cook',
@@ -61,7 +60,7 @@ app.get('/logout', async (req, res) => {
 app.post('/register', async (req, res) => {
   try {
     const {
-      email, password, name, 
+      email, password, name,
     } = req.body;
     const result = await User.create({
       email, password: await Bcrypt.hash(password), name,

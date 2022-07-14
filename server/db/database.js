c780b49c3cb75bef7444245dedbdf07a8547ccb3
tymphoto <9256187876@mail.ru>
2022-07-14 17:27:27 +0300

    db config for heroku

--- server/db/database.js
+++ server/db/database.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: '127.0.0.1',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
   },
   test: {
@@ -15,10 +16,13 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
   },
 };
